b4655fd1b57854c3cc51722170c684c583648d52
georgetegral <jorgerenemg@icloud.com>
2022-02-21 11:47:50 -0600

    Added web3 in UseEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/home/index.js';
+import Web3 from 'web3';
 
 function App() {
+  useEffect(() => {
+    if(window.ethereum) {
+      // window.ethereum
+      //   .request({
+      //     method: "eth_requestAccounts",
+      //   })
+      //   .then((accounts) => console.log(accounts))
+      const web3 = new Web3(window.ethereum);
+      web3.eth.requestAccounts().then(console.log());
+    }
+  }, []);
+
   return (
     <>
     <Routes>
       <Route path="/" element={<Home />}/>
     </Routes>
     </>
-  );
+  ); 
 }
 
 export default App;
