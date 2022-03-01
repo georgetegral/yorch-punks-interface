d765f376a1482160f2492beaabf76c9db7c6a898
georgetegral <jorgerenemg@icloud.com>
2022-03-01 13:52:49 -0600

    Added mint functionality

--- src/hooks/usePlatziPunks/index.js
+++ src/hooks/usePlatziPunks/index.js
@@ -1,15 +1,15 @@
 import { useMemo } from 'react';
 import { useWeb3React } from '@web3-react/core';
-import { PlatziPunksArtifact} from "../../config/web3/artifacts/PlatziPunks";
+import PlatziPunksArtifact from '../../config/web3/artifacts/PlatziPunks';
 
-const { address, abi} = PlatziPunksArtifact;
+const { address, abi } = PlatziPunksArtifact;
 
 const usePlatziPunks = () => {
-    const {active, library, chainId} = useWeb3React();
+    const { active, library, chainId } = useWeb3React();
 
     const platziPunks = useMemo(() => {
         if(active) return new library.eth.Contract(abi, address[chainId]);
-    }, [chainId, library?.eth?.Contract]);
+    }, [active, chainId, library?.eth?.Contract]);
     return platziPunks;
 };
 
